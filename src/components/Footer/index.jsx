518aa12f0fffaf3bd06643fa5f28a5b3ff135835
LoveHope5 <dimitrimehelo@gmail.com>
2022-09-29 21:16:40 +0100

    footer layout

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import  './Footer.css'
 
 const Footer = () => {
-  return <div> Footer</div>;
+  return (<>
+    <div id="footer">
+      <div class="footer-top">
+            <div class="footer-col">
+                    <h3>Gomerce</h3>
+                    
+            </div>
+            <div class="footer-col">
+                  <h3>About</h3>
+                  <ul>
+                    <li>How to register</li>
+                    <li>Forgot Password</li>
+                    <li>Blog</li>
+                    <li>Forum</li>
+                  </ul>
+            </div>
+            <div class="footer-col">
+                  Partner with us
+            </div>
+            <div class="footer-col">
+                  Support
+            </div>
+      </div>
+      <div class="footer-bottom">
+        copyright
+      </div>
+    </div>
+
+  </>
+  );
 };
 
 export default Footer;
---
+++ src/components/FooterColumn/FooterColumn.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './FooterColumn.css'
+
+const FooterColumn = (props) =>{
+
+   
+
+    return(
+        <>
+            <div class="footer-col">
+                    <h3>props.title</h3>
+                    
+            </div>
+        </>
+    )
+}
