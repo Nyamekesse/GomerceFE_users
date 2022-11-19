8666af1b0c0a08e34dcd70f94219b153774e880e
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2022-11-19 18:18:56 +0000

    updated codebase for corrections

--- src/Views/Views.jsx
+++ src/Views/Views.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "../pages/LandingPage/LandingPage";
 import Cart from "../pages/Cart";
-import CheckoutPayment from "../pages/CheckoutPayment/CheckoutPayment";
+import CheckoutPayment from "../pages/CheckoutPayment";
+import CheckoutDelivery from "../pages/CheckoutDelivery/CheckoutDelivery";
 import ItemPage from "../pages/ItemPage";
 import LogIn from "../pages/LogIn";
 import OrdersDetail from "../pages/OrdersDetail";
@@ -19,6 +20,7 @@ const Views = () => {
       <Route exact path="/cart" element={<Cart />} />
       <Route exact path="/checkout-address" element={<CheckoutAddress />} />
       <Route exact path="/checkout-payment" element={<CheckoutPayment />} />
+      <Route exact path="/checkout-delivery" element={<CheckoutDelivery />} />
       <Route exact path="/item-page" element={<ItemPage />} />
       <Route exact path="/login" element={<LogIn />} />
       <Route exact path="/order-detail" element={<OrdersDetail />} />
