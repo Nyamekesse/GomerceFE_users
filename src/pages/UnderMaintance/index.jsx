a2f07da612ea787dbde7e355d614a2e8caabaa21
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2022-12-29 16:35:16 +0000

    added and completed maintenance page

---
+++ src/pages/UnderMaintance/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Container, Wrap, Image } from "./styles";
+import maintenancePage from "../../assets/images/page-under-maintenance.png";
+import { Typography, Box, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+const PageNotFound = () => {
+  const navigate = useNavigate();
+  return (
+    <Wrap>
+      <Container>
+        <Image src={maintenancePage} alt={maintenancePage} />
+        <Typography
+          variant="body1"
+          fontSize={{ mobile_0: 30, mobile_393: 40 }}
+          fontWeight={400}
+          mb={{ mobile_0: 0, mobile_393: 1 }}
+        >
+          Under maintenance
+        </Typography>
+        <Box
+          width={{ mobile_0: "100%", tablet_600: "419px" }}
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          textAlign={"center"}
+          margin={"0 auto"}
+        >
+          <Typography variant="caption" fontSize={12} fontWeight={400} mb={1}>
+            This page is under maintenance. Please try again after 30 minutes
+          </Typography>
+        </Box>
+        <Button variant="contained" onClick={() => navigate("/")}>
+          take me back to homepage
+        </Button>
+      </Container>
+    </Wrap>
+  );
+};
+
+export default PageNotFound;
