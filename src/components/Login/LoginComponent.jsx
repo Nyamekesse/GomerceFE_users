122681501ad2b9b430a4a287b8f11dfb103f5b07
fodela <particleme30@gmail.com>
2023-01-05 14:48:31 +0000

    styles login component

--- src/components/Login/LoginComponent.jsx
+++ src/components/Login/LoginComponent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import { Button, InputLabel, Link, TextField, Typography } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
-
+import "./styles.css";
 const LoginComponent = () => {
   // References
   const emailRef = useRef();
@@ -16,9 +16,9 @@ const LoginComponent = () => {
 
   // Autofocus
 
-  useEffect(() => {
-    emailRef.current.getElementsByTagName("input")[0].focus();
-  }, []);
+  // useEffect(() => {
+  //   emailRef.current.getElementsByTagName("input")[0].focus();
+  // }, []);
 
   const handleSubmit = async (event) => [
     console.log(
@@ -26,51 +26,62 @@ const LoginComponent = () => {
     ),
   ];
   return (
-    <Grid container direction="row" sx={{ height: "100vh" }}>
+    <Grid
+      container
+      alignContent="center"
+      justifyContent="center"
+      sx={{
+        height: "100vh",
+        backgroundImage: "url(images/signin.jpg)",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <Grid item mobile_0={0} mobile_550={5} tablet_840={6} laptop_1024={8} />
+
       <Grid
         item
-        lg={8}
-        md={8}
-        sm={6}
-        sx={{
-          backgroundImage: "url(Images/login.jpg)",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      />
-
-      <Grid item alignSelf="center" lg={4} md={4} sm={6} xs={12} sx={{ p: 4 }}>
-        <Grid container direction="column" alignItems="center" rowGap={2}>
-          <Typography variant="h5">Welcome back</Typography>
-          <Typography variant="body2">Please enter your details</Typography>
-        </Grid>
-        <Grid container component="form" rowGap={4} onSubmit={handleSubmit}>
-          <Grid container direction="column">
-            <InputLabel>Email</InputLabel>
-            <TextField
-              size="small"
-              required
-              fullWidth
-              ref={emailRef}
-              type="email"
-              autoComplete="email"
-              autoFocus
-            />
-          </Grid>
-          <Grid container direction="column">
-            <InputLabel>Password</InputLabel>
-            <TextField size="small" required fullWidth type="password" />
+        sx={{ background: "white", p: 4 }}
+        mobile_0={10}
+        mobile_550={7}
+        tablet_840={6}
+        laptop_1024={4}
+      >
+        <Grid container className="full_height550" alignContent="center">
+          <Grid container direction="column" alignItems="center" rowGap={2}>
+            <Typography variant="h5">Welcome back</Typography>
+            <Typography variant="body2">Please enter your details</Typography>
           </Grid>
 
-          <Grid container direction="column" rowGap={2} alignItems="center">
-            <Button variant="contained" type="submit" sx={{ px: 6 }}>
-              Sign in
-            </Button>
+          <Grid container component="form" rowGap={4} onSubmit={handleSubmit}>
+            <Grid container direction="column">
+              <InputLabel>Email</InputLabel>
+              <TextField
+                size="small"
+                required
+                fullWidth
+                ref={emailRef}
+                type="email"
+                autoComplete="email"
+                autoFocus
+              />
+            </Grid>
 
-            <Typography variant="body2">
-              Don't have an account? <Link href="/signup">Sing up</Link>
-            </Typography>
+            <Grid container direction="column">
+              <InputLabel>Password</InputLabel>
+              <TextField size="small" required fullWidth type="password" />
+            </Grid>
+
+            <Grid container direction="column" rowGap={2} alignItems="center">
+              <Button variant="contained" type="submit" sx={{ px: 6 }}>
+                Sign in
+              </Button>
+
+              <Typography variant="body2">
+                Don't have an account? <Link href="/signup">Sing up</Link>
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
